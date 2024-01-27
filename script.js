503d9eda4f20d15a4df15db9d443656558409922
PrABpY <86980431+PrABpY@users.noreply.github.com>
2024-01-27 10:21:58 +0700

    ก

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let text;
 let userpinlock = 0;
+let ip = "http://192.168.72.154";
 if (userpinlock == 0) {
-    text = "Lock"
-    document.getElementById("demo").innerHTML = text;
+    document.getElementById("myImg").src = "image/lock.png";
+    document.getElementById("bimg").src = "image/block.png";
 }
 function getHello() {
     if (userpinlock == 1) {
-        const url = 'http://192.168.232.154:80/home'
-        location.href = "http://192.168.232.154:8989/AMR-UI/lock.html";
+        const url = ip+':80/home'
+        location.href = "lock.html";
         fetch(url, {headers: new Headers({"ngrok-skip-browser-warning": "69420",})})
         .then(response => response.text())  
         .then(text => {
@@ -18,13 +19,13 @@ function getHello() {
     if (userpinlock == 0) {
         let person = prompt("Please enter your PIN:");
         if (person == "12345") {
-            location.href = "http://192.168.232.154:8989/AMR-UI/lock.html";
+            location.href = "lock.html";
         } 
     }
     
 }
 function getHello_2() {
-    const url = 'http://192.168.232.154:80/hello'
+    const url = ip+':80/hello'
     fetch(url, {
       method: "get",
       headers: new Headers({
@@ -40,21 +41,24 @@ function getHello_2() {
 function test() {
     let i = 0;
     while (i < 500) {
-      const url = 'http://192.168.232.154:80/test'
+      const url = ip+':80/test'
       fetch(url)
       i++;
     }
     
 }
 function button_pin() {
-  let person = prompt("Please enter your PIN:");
-  if (person == "12345") {
-    text = "Unlock";
-    userpinlock = 1;
-  } else {
-    text = "Lock";
-  }
-  document.getElementById("demo").innerHTML = text;
+  if (userpinlock == 0){
+      let person = prompt("Please enter your PIN:");
+      if (person == "12345") {
+        document.getElementById("myImg").src = "image/unlock.png";
+        document.getElementById("bimg").src = "image/bunlock.png";
+        userpinlock = 1;
+      } else {
+        document.getElementById("myImg").src = "image/lock.png";
+        document.getElementById("bimg").src = "image/block.png";
+      }
+}
 }
 
 
