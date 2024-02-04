ec2559552fe3bbd7694e0082be8f5d3aa82e9e2e
PrABpY <86980431+PrABpY@users.noreply.github.com>
2024-02-04 15:44:04 +0700

    edit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let text;
 let userpinlock = 0;
 let link = window.location.href;
+let pin = "123"
 const ip = link.split(":");
 // document.getElementById("demo").innerHTML = "http:"+ip[1]+':80/home'
 // document.getElementById("bimg").src = "image/block.png";
@@ -11,6 +12,42 @@ if (userpinlock == 0) {
 
 function to_delivery() {
     location.href = "delivery.html";
+}
+
+function to_guide() {
+    location.href = "guide.html";
+}
+
+function to_control() {
+    location.href = "control.html"
+}
+
+function control() {
+    if (userpinlock == 1) {
+        setTimeout(to_control, 500);
+    }
+    if (userpinlock == 0) {
+        let person = prompt("Please enter your PIN:");
+        if (person == pin) {
+            userpinlock = 1
+            document.getElementById("bimg").src = "image/bunlock.png";
+            setTimeout(to_control, 500);
+        } 
+    }
+}
+
+function guide() {
+    if (userpinlock == 1) {
+        setTimeout(to_guide, 500);
+    }
+    if (userpinlock == 0) {
+        let person = prompt("Please enter your PIN:");
+        if (person == pin) {
+            userpinlock = 1
+            document.getElementById("bimg").src = "image/bunlock.png";
+            setTimeout(to_guide, 500);
+        } 
+    }
 }
 
 function delivery() {
@@ -27,7 +64,7 @@ function delivery() {
     }
     if (userpinlock == 0) {
         let person = prompt("Please enter your PIN:");
-        if (person == "12345") {
+        if (person == pin) {
             userpinlock = 1
             document.getElementById("bimg").src = "image/bunlock.png";
             const url = "http:"+ip[1]+':80/delivery'
@@ -58,7 +95,7 @@ function test() {
 function button_pin() {
   if (userpinlock == 0){
       let person = prompt("Please enter your PIN:");
-      if (person == "12345") {
+      if (person == pin) {
         document.getElementById("myImg").src = "image/unlock.png";
         document.getElementById("bimg").src = "image/bunlock.png";
         userpinlock = 1;
